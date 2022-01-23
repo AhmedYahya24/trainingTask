155d997509d969d093b9852a44a625ef5f6c600c
AhmedYahya24 <ahmed1998yahia@gmail.com>
2022-01-23 18:51:08 +0200

    'initial'

---
+++ train/src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className='firstRow'>
+        <div className='row'></div>
+        <div className='row'></div>
+        <div className='row'></div>
+        <div className='row'></div>
+        <div className='row'></div>
+        <div className='row'></div>
+        <div className='row'></div>
+      </div>
+      <div className='columnContainer'>
+        <div className='firstColumn'>
+          <div className='column'></div>
+          <div className='column'></div>
+          <div className='column'></div>
+        </div>
+        <div className='secondcolumn'>
+          <div className='column'></div>
+          <div className='column'></div>
+          <div className='column'></div>
+        </div>
+      </div>
+      <div className='secondRow'>
+        <div className='row'></div>
+        <div className='row'></div>
+        <div className='row'></div>
+        <div className='row'></div>
+        <div className='row'></div>
+        <div className='row'></div>
+        <div className='row'></div>
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
